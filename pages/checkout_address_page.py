6e1aa003895d45eec58b70405655b8d9b745e76d
“unaisLearning” <unais.learning@gmail.com>
2026-07-06 05:43:04 +0530

    Added Github automation flows

--- pages/checkout_address_page.py
+++ pages/checkout_address_page.py
@@ -13,10 +13,10 @@ class CheckoutAddressPage:
     def get_title(self):
         return self.page.locator(self.TITLE)
 
-    def fill_address_form(self, FIRST_NAME,LAST_NAME,ZIP_CODE):
-       self.page.locator(self.FIRST_NAME_LOCATOR).fill(FIRST_NAME)
-       self.page.locator(self.LAST_NAME_LOCATOR).fill(LAST_NAME)
-       self.page.locator(self.ZIP_CODE_LOCATOR).fill(ZIP_CODE)
+    def fill_address_form(self, first_name, last_name, zip_code):
+        self.page.locator(self.FIRST_NAME_LOCATOR).fill(first_name)
+        self.page.locator(self.LAST_NAME_LOCATOR).fill(last_name)
+        self.page.locator(self.ZIP_CODE_LOCATOR).fill(zip_code)
 
     def proceed_to_overview_page(self):
         self.page.locator(self.CONTINUE_BUTTON).click()
